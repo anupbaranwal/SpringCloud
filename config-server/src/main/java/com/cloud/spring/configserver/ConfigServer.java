2e53faf88dc84deee74eca6ff78570cb78921053
Anup Kumar <anup.raj1989@gmail.com>
2017-05-14 21:14:52 +0530

    Initial Commit

---
+++ config-server/src/main/java/com/cloud/spring/configserver/ConfigServer.java
@@ -0,0 +1,18 @@
+package com.cloud.spring.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by Anup on 14-05-2017.
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
